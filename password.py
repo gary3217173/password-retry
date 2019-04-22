bebeeb3a5f61924d52fa46bcbeda2c34732334c9
Gary Chen <gary3217173@gmail.com>
2019-04-22 20:58:24 +0800

    剩餘0次不要印

--- password.py
+++ password.py
@@ -1,9 +1,11 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		break # 逃出迴圈
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
